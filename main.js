3049ade8ed59549d2f669c297928eb5194324bcf
Bridget Lawani <105175902+Bridgetlawani@users.noreply.github.com>
2022-06-22 00:23:27 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+let input1 = prompt("Enter your name")
+let input2 = prompt("Enter your height")
+let input3 = prompt("Enter your country")
+
+document.write("Name: " + input1  + " " + "height: " + input2 + " " + "Country: "  + input3)
